9bf120e59099d967fb59e03da313560ae58d4aab
Vokerlee <vokerlee@gmail.com>
2023-09-15 00:45:44 +0300

    Add intro-task 3 implementation

---
+++ task_intro_3/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <err.h>
+#include <sysexits.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    omp_set_num_threads(omp_get_num_procs());
+
+    long int sum = 0;
+
+    #pragma omp parallel for ordered
+        for (size_t i = 0; i < omp_get_num_threads(); ++i)
+        {
+            #pragma omp ordered
+            sum += 1;
+
+            printf("%d %ld\n", omp_get_thread_num(), sum);
+        }
+
+    printf("Total sum is %ld\n", sum);
+
+    return EXIT_SUCCESS;
+}
